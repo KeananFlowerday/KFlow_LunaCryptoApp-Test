22224f25bec98c1c500938a827a66a4b9a48fa3f
Keanan Tyde Flowerday <k.flowerday@gmail.com>
2021-10-14 20:22:05 +0200

    getting data from API's

--- index.js
+++ index.js
@@ -5,7 +5,46 @@ const PORT = 5000;
 const app = express();
 
 
+async function calculateMarkets(){
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
 
+    //Valr
+var valrData = await fetch("https://api.valr.com/v1/public/marketsummary", requestOptions)
+    .then(valrRes => valrRes.json())
+    .then(data => valrData = data)
+    .then(() => console.log('valrData'))       
+    .catch(error => console.log('error', error));
+
+//Chainex
+var chainexData = await fetch("https://api.chainex.io/market/summary/", requestOptions)
+.then(chainexRes => chainexRes.json())
+.then(data => chainexData = data)
+.then(() => console.log('chainexData'))       
+.catch(error => console.log('error', error));
+
+var shareData ;
+
+
+
+/*    for(vitem in valrData){
+        console.log(valrData[vitem])
+        for(citem in chainexData){
+            console.log(valrData[vitem]);
+            if(
+                valrData[vitem].currencyPair  === (chainexData[citem].code+chainexData[citem].exchange) || 
+                valrData[vitem].currencyPair  === (chainexData[citem].exchange+chainexData[citem].code))
+                {
+                    shareData.push({valrItem: valrData[vitem], chainexItem: chainexData[citem]});
+                };
+        }
+    }
+*/
+return shareData
+
+}
 
 app.get('/HealthCheck', (req, res) => {
     res.send({Status: '1'});
@@ -38,23 +77,9 @@ app.get('/chainex', (req, res) => {
 
 
 app.get('/', (req, res) => {
-    const valrRes = await fetch('https://api.valr.com/v1/public/marketsummary');
-    var valrData = [await valrRes.json()];
-
-    const chainexRes = await fetch('https://api.chainex.io/market/summary/');
-    var chainexData = [await chainexRes.json()];
-
-    var shareData = [];
-
-    for(vitem in valrData){
-        for(citem in chainexData){
-            if(valrData[vitem].currencyPair == (chainexData[citem].code+chainexData[citem].exchange) && valrData[vitem]. &&){
-        shareData.push();
-            };
-        }
-    }
-
-    res.send({Hey: 'World'});
+    
+    
+    res.send(calculateMarkets());
 
 });
 
