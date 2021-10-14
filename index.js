93b4af116e9b7c7147dbc0adb1aeac2ff67410b8
Keanan Tyde Flowerday <k.flowerday@gmail.com>
2021-10-14 21:09:58 +0200

    refactor

--- index.js
+++ index.js
@@ -4,28 +4,48 @@ const PORT = 5000;
 
 const app = express();
 
-
-async function calculateMarkets(){
+async function getChainex()
+{
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
 
-    //Valr
-var valrData = await fetch("https://api.valr.com/v1/public/marketsummary", requestOptions)
-    .then(valrRes => valrRes.json())
-    .then(data => valrData = data)
-    .then(() => console.log('valrData'))       
-    .catch(error => console.log('error', error));
-
 //Chainex
 var chainexData = await fetch("https://api.chainex.io/market/summary/", requestOptions)
 .then(chainexRes => chainexRes.json())
 .then(data => chainexData = data)
-.then(() => console.log('chainexData'))       
+.then(() => console.log('chainexData'))  
+.then(() => {return chainexData})     
 .catch(error => console.log('error', error));
+}
 
+async function getValr()
+{
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+//Valr
+var valrData = await fetch("https://api.valr.com/v1/public/marketsummary", requestOptions)
+    .then(valrRes => valrRes.json())
+    .then(data => valrData = data)
+    .then(() => console.log('valrData')) 
+    .then(() => {return '1'})      
+    .catch(error => console.log('error', error));
+}
+
+
+async function calculateMarkets(){
+
+const valrInfo = await getValr().then(() => console.log(valrInfo))
+    .catch(error => console.log('error', error));
+const chainexInfo = await getChainex()
+    .catch(error => console.log('error', error));
 var shareData ;
+
+console.log(valrInfo);
 
 
 
